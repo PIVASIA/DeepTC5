06c9828b13dd51f380b23820274c5831eecd78c6
Hung Luu <hunglv@fimo.edu.vn>
2019-06-15 22:33:44 +0900

    fix filepath

--- keras_retinanet/preprocessing/csv_generator.py
+++ keras_retinanet/preprocessing/csv_generator.py
@@ -25,7 +25,7 @@ from six import raise_from
 import csv
 import sys
 import os.path
-
+from pathlib import Path
 
 def _parse(value, function, fmt):
     """
@@ -192,7 +192,7 @@ class CSVGenerator(Generator):
     def image_path(self, image_index):
         """ Returns the image path for image_index.
         """
-        return os.path.join(self.base_dir, self.image_names[image_index])
+        return Path(os.path.join(self.base_dir, self.image_names[image_index]))
 
     def image_aspect_ratio(self, image_index):
         """ Compute the aspect ratio for an image with image_index.
