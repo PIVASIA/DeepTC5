9afd60ad2dca41b74bb5b058ee5416e5a05864ed
hunglv <hunglv@fimo.edu.vn>
2019-06-17 12:18:28 +0900

    save model by save_best_only

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -186,9 +186,10 @@ def create_callbacks(model, training_model, prediction_model, validation_generat
                 '{backbone}_{dataset_type}_{{epoch:02d}}.h5'.format(backbone=args.backbone, dataset_type=args.dataset_type)
             ),
             verbose=1,
-            # save_best_only=True,
-            # monitor="mAP",
-            # mode='max'
+            # period=10,
+            save_best_only=True,
+            monitor="mAP",
+            mode='max'
         )
         checkpoint = RedirectModel(checkpoint, model)
         callbacks.append(checkpoint)
